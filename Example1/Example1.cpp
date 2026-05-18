ea3ee561db827e6d2de449ac4fa4285c35bdf9e0
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-05-18 01:36:34 +0100

    Add project files.

---
+++ Example1/Example1.cpp
@@ -0,0 +1,64 @@
+#include <chrono>
+#include <iostream>
+
+
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00
+#endif
+#define ASIO_STANDALONE
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
+int main() {
+
+	asio::error_code ec;
+
+	// Creating a context - platform specific interface.
+	asio::io_context context;
+
+	// Get address of somewhere we want to connect to: - endpoint is an address.
+	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("127.0.0.1", ec), 80);
+
+	// Creating a socket:
+	asio::ip::tcp::socket socket(context);
+
+	// Tell socket to try & connect
+	socket.connect(endpoint, ec);
+
+	if (!ec)
+	{
+		std::cout << "Connected!" << std::endl;
+	}
+	else 
+	{
+		std::cout << "Failed to connect to address:\n" << ec.message() << std::endl;
+	}
+
+	if (socket.is_open())
+	{
+		std::string sRequest =
+			"GET /index.html HTTP/1.1\r\n"
+			"Host: example.com\r\n"
+			"Connection: close\r\n\r\n";
+
+		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
+
+		socket.wait(socket.wait_read);
+
+		size_t bytes = socket.available();
+		std::cout << "Bytes Available: " << bytes << std::endl;
+
+		if (bytes > 0)
+		{
+			std::vector<char> vBuffer(bytes);
+			socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
+
+			for (auto c : vBuffer)
+				std::cout << c;
+		}
+	}
+
+	return 0;
+	
+}
