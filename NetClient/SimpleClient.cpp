fc2c3303e412eaab61dc08788f207bf79e5a4569
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-05-22 19:32:51 +0100

    Updated SimpleClient.cpp

--- NetClient/SimpleClient.cpp
+++ NetClient/SimpleClient.cpp
@@ -15,20 +15,74 @@ enum class CustomMsgTypes : uint32_t
 
 class CustomClient : public olc::net::client_interface<CustomMsgTypes>
 {
+public:
+	void PingServer()
+	{
+		olc::net::message<CustomMsgTypes> msg;
+		msg.header.id = CustomMsgTypes::ServerPing;
 
+		// Grab timepoint:
+		std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
+
+		msg << timeNow;
+		Send(msg);
+	}
 };
 
 int main()
 {
-	std::cout << "Client starting...\n";
+	
 
 	CustomClient c;
+	c.Connect("127.0.0.1", 60000);
 
-	std::cout << "Trying to connect...\n";
-	c.Connect("community.onelonecoder.com", 60000);
+	bool key[3] = { false, false, false };
+	bool old_key[3] = { false, false, false };
 
-	std::cout << "Client running. Press enter to exit...\n";
-	std::cin.get();
+	bool bQuit = false;
+	while (!bQuit)
+	{
+		if (GetForegroundWindow() == GetConsoleWindow())
+		{
+			key[0] = GetAsyncKeyState('1') & 0x8000;
+			key[1] = GetAsyncKeyState('2') & 0x8000;
+			key[2] = GetAsyncKeyState('3') & 0x8000;
+		}
+
+		if (key[0] && !old_key[0]) c.PingServer();
+		if (key[2] && !old_key[2]) bQuit = true;
+
+		for (int i = 0; i < 3; i++) old_key[i] = key[i];
+
+		if (c.IsConnected())
+		{
+			if (!c.Incoming().empty())
+			{
+				auto msg = c.Incoming().pop_front().msg;
+
+				switch (msg.header.id)
+				{
+				case CustomMsgTypes::ServerPing:
+				{
+					std::chrono::system_clock::time_point timeNow = std::chrono::system_clock::now();
+					std::chrono::system_clock::time_point timeThen;
+					msg >> timeThen;
+					std::cout << "Ping: " << std::chrono::duration<double>(timeNow - timeThen).count() << "\n";
+				}
+				break;
+				}
+
+			}
+		}
+		else
+		{
+			std::cout << "Server Down\n";
+			bQuit = true;
+		}
+	}
+
+
+
 
 	return 0;
 }
