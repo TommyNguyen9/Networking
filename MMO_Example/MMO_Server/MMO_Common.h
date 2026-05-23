a028c1801ffe02e1d1b93415a573b774a98fe499
TommyNguyen9 <tommynguyen3458@gmail.com>
2026-05-23 19:49:13 +0100

    Added MMO_Example game

---
+++ MMO_Example/MMO_Server/MMO_Common.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+#define OOLC_PGEX_NETWORK
+#include "olcPGEX_Network.h"
+
+enum class GameMsg : uint32_t
+{
+	Server_GetStatus,
+	Server_GetPing,
+
+	Client_Accepted,
+	Client_AssignID,
+	Client_RegisterWithServer,
+	Client_UnregisterWithServer,
+
+	Game_AddPlayer,
+	Game_RemovePlayer,
+	Game_UpdatePlayer,
+};
